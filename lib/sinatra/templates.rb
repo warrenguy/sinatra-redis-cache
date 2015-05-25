6117ef10d5561bba63c0870b2693604f19043ca8
Warren Guy <warren@guy.net.au>
2015-05-26 00:56:51 +0100

    remove redundant check

--- lib/sinatra/templates.rb
+++ lib/sinatra/templates.rb
@@ -7,7 +7,7 @@ module Sinatra
       # args [0]=engine [1]=data [2]=options [3]=locals
       key = "template/#{args[0..1].join('/')}/#{Digest::MD5.hexdigest(args[0..3].to_s)}"
 
-      if !settings.nil? and defined?(settings.redis_cache_automatic) and settings.redis_cache_automatic
+      if defined?(settings.redis_cache_automatic) and settings.redis_cache_automatic
         cache_do(key) { orig_render(*args, &block) }
       else
         orig_render(*args, &block)
