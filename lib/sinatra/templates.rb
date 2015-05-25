37e937937dfd8d7248dcb7f03023ed8dc7a83bb8
Warren Guy <warren@guy.net.au>
2015-05-25 23:39:24 +0100

    generate more sensible key for auto caching

--- lib/sinatra/templates.rb
+++ lib/sinatra/templates.rb
@@ -4,7 +4,9 @@ module Sinatra
     alias_method :orig_render, :render
 
     def render(*args, &block)
-      key = 'auto/' + Digest::SHA256.hexdigest(args.to_s)
+      # args [0]=engine [1]=data [2]=options [3]=locals
+      key = "template/#{args[0..1].join('/')}/#{Digest::MD5.hexdigest(args[0..3].to_s)}"
+
       if !settings.nil? and defined?(settings.redis_cache_automatic) and settings.redis_cache_automatic
         cache_do(key) { orig_render(*args, &block) }
       else
